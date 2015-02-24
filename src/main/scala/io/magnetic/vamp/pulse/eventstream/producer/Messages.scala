433b1a9154bd11750a75fe32c2e5abbbd769fff9
Roman Useinov <roman@magnetic.io>
2015-02-24 13:54:02 +0100

    default date for a metric + external elasticsearch by default

--- src/main/scala/io/magnetic/vamp/pulse/eventstream/producer/Messages.scala
+++ src/main/scala/io/magnetic/vamp/pulse/eventstream/producer/Messages.scala
@@ -2,4 +2,4 @@ package io.magnetic.vamp.pulse.eventstream.producer
 
 import java.util.Date
 
-final case class Metric(name: String, value: Int, timestamp: Date)
+final case class Metric(name: String, value: Int, timestamp: Date = new Date())
