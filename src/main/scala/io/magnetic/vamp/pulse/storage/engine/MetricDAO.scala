352ed4969e9079a925286b78c12fd0203457ce7d
Roman Useinov <roman@magnetic.io>
2015-02-26 14:45:09 +0100

    switched mutable list for queue

--- src/main/scala/io/magnetic/vamp/pulse/storage/engine/MetricDAO.scala
+++ src/main/scala/io/magnetic/vamp/pulse/storage/engine/MetricDAO.scala
@@ -9,7 +9,7 @@ import io.magnetic.vamp.pulse.eventstream.producer.Metric
 import io.magnetic.vamp.pulse.mapper.CustomObjectSource
 import io.magnetic.vamp.pulse.eventstream.decoder.MetricDecoder
 
-import scala.collection.mutable
+import scala.collection.mutable.Queue
 import scala.concurrent.ExecutionContext
 
 
@@ -27,8 +27,11 @@ class MetricDAO(implicit client: ElasticClient, implicit val executionContext: E
   //TODO: Figure out timestamp issues with elastic: We can only use epoch now + we get epoch as a double from elastic.
   def getMetrics(metricQuery: MetricQuery) = {
     val shouldMatch = metricQuery.tags.length
-    val queries: mutable.MutableList[QueryDefinition] = mutable.MutableList(rangeQuery("timestamp") from metricQuery.time.from.toEpochSecond to metricQuery.time.to.toEpochSecond)
+
+    val queries: Queue[QueryDefinition] = Queue(rangeQuery("timestamp") from metricQuery.time.from.toEpochSecond to metricQuery.time.to.toEpochSecond)
+
     if(metricQuery.tags.length > 0) queries += termsQuery("tags", metricQuery.tags:_*) minimumShouldMatch(shouldMatch)
+
     client.execute {
       search in ind -> entity query {
         must  (
