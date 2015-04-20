b92c2d1ec7ed99b9a018a6bb3cd6c84ebda6678d
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-04-20 17:29:20 +0200

    expanding tags

--- src/main/scala/io/vamp/pulse/eventstream/message/Events.scala
+++ src/main/scala/io/vamp/pulse/eventstream/message/Events.scala
@@ -8,12 +8,14 @@ import io.vamp.pulse.api.Event
 import io.vamp.pulse.util.Serializers
 import org.json4s.ext.EnumNameSerializer
 
+import scala.language.postfixOps
 import scala.util.Try
 
 object EventType extends Enumeration {
   type EventType = Value
   val Numeric, JsonBlob, Typed = Value
 }
+
 import io.vamp.pulse.eventstream.message.EventType._
 
 class EventTypeRef extends TypeReference[EventType.type]
@@ -22,12 +24,12 @@ class EventTypeRef extends TypeReference[EventType.type]
 final case class Metric(tags: Seq[String], value: Double, timestamp: OffsetDateTime = OffsetDateTime.now())
 
 
-final case class ElasticEvent(tags: Seq[String], value: AnyRef, timestamp: OffsetDateTime, properties: EventProperties, blob: AnyRef = ""){
+final case class ElasticEvent(tags: Seq[String], value: AnyRef, timestamp: OffsetDateTime, properties: EventProperties, blob: AnyRef = "") {
   def convertOutput = {
     this match {
       case ElasticEvent(_, v: Map[_, _], _, props, _)
         if props.`type` == EventType.Numeric =>
-          Try(Metric(tags, v.asInstanceOf[Map[String, Double]]("numeric"), timestamp)).getOrElse(Metric(tags, 0D, timestamp))
+        Try(Metric(tags, v.asInstanceOf[Map[String, Double]]("numeric"), timestamp)).getOrElse(Metric(tags, 0D, timestamp))
 
       case ElasticEvent(_, _, _, props, b) if props.`type` == EventType.JsonBlob => Event(tags, b, timestamp)
 
@@ -36,26 +38,33 @@ final case class ElasticEvent(tags: Seq[String], value: AnyRef, timestamp: Offse
   }
 }
 
-final case class EventProperties(@JsonScalaEnumeration(classOf[EventTypeRef])`type`: EventType, objectType: String = "")
-
-
-
+final case class EventProperties(@JsonScalaEnumeration(classOf[EventTypeRef]) `type`: EventType, objectType: String = "")
 
 
 object ElasticEvent {
-  implicit val formats = Serializers.formats +  new EnumNameSerializer(EventType)
+  implicit val formats = Serializers.formats + new EnumNameSerializer(EventType)
 
+  private val tagDelimiter = ':'
+
+  def expandTags: (Seq[String] => Seq[String]) = { (tags: Seq[String]) =>
+    tags.flatMap { tag =>
+      tag.indexOf(tagDelimiter) match {
+        case -1 => tag :: Nil
+        case index => tag.substring(0, index) :: tag :: Nil
+      }
+    } distinct
+  }
 
   implicit def metricToElasticEvent(metric: Metric): ElasticEvent = {
-    ElasticEvent(metric.tags, Map("numeric" -> metric.value), metric.timestamp, EventProperties(EventType.Numeric))
+    ElasticEvent(expandTags(metric.tags), Map("numeric" -> metric.value), metric.timestamp, EventProperties(EventType.Numeric))
   }
 
 
   implicit def eventToElasticEvent(event: Event): ElasticEvent = {
-    if(event.`type`.isEmpty) {
-      ElasticEvent(event.tags, Map.empty, event.timestamp, EventProperties(EventType.JsonBlob), event.value)
+    if (event.`type`.isEmpty) {
+      ElasticEvent(expandTags(event.tags), Map.empty, event.timestamp, EventProperties(EventType.JsonBlob), event.value)
     } else {
-      ElasticEvent(event.tags, Map(event.`type` -> event.value), event.timestamp, EventProperties(EventType.Typed, event.`type`))
+      ElasticEvent(expandTags(event.tags), Map(event.`type` -> event.value), event.timestamp, EventProperties(EventType.Typed, event.`type`))
     }
 
   }
